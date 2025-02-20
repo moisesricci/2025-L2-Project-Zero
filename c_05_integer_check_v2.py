ad5540d9cd0475a8b2e378e76a25421e4c150569
moisesricci <moises.machadoricci@horowhenua.school.nz>
2025-02-20 13:20:45 +1300

    Integer more than zero / between two values

---
+++ c_05_integer_check_v2.py
@@ -0,0 +1,29 @@
+# Functions go here
+def int_check(question, low, high):
+    """Checks users enter an integer between two values"""
+
+    error = f"Oops -= please enter and integer between {low} and {high}."
+
+    while True:
+
+        try:
+            # change the response to an integer and check that it's more than zero
+            response = int(input(question))
+
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+# Main Routine goes here
+
+# loop for testing purposes...
+while True:
+    print()
+
+    # ask user for an integer
+    my_num = int_check(question="Choose a number: ", low=1, high=10)
+    print(f"You chose {my_num}")
